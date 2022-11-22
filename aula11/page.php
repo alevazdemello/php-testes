490a2f55009701cfcb7d8e1a24ead073b4d17dca
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2022-11-21 21:30:16 -0300

    Contagem Progressiva
    Contagem Progressiva

---
+++ aula11/page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contagem Progressiva</title>
+</head>
+
+<body>
+    <h1>Contagem Progressiva</h1>
+    <p>Este programa faz a contagem progressiva de 0 a 10 usando PHP.</p>
+    <br />
+    <?php
+    $c = 1;
+    while ($c <= 10) {
+        echo "$c <br />";
+        $c ++;
+    }
+
+
+
+
+    ?>
+
+    <br />
+    <br />
+    <a href="index.html">Voltar</a>
+</body>
+
+</html>
